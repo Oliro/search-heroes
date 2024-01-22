436bb11f3ce60999f55fe46c2f5f54e7fa675ade
Fernando OR <fernando.or6@gmail.com>
2024-01-22 11:35:58 -0300

    feat: create search component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchComponent } from '../shared/search/search.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
---
+++ src/app/shared/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent {
+
+  public query: string = '';
+  
+  search(){
+
+  }
+
+}
