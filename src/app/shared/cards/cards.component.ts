056c1f177844ee0de083fdde41281481111e65d1
Fernando OR <fernando.or6@gmail.com>
2024-01-23 13:19:04 -0300

    feat: add  cards style and icons

--- src/app/shared/cards/cards.component.ts
+++ src/app/shared/cards/cards.component.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Character } from 'src/app/models/character';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
 
 @Input() character!: Character;
+
+public isFavorite: boolean = false;
+
+favorite(){
+this.isFavorite = !this.isFavorite;
+}
 
 }
