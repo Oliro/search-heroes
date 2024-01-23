fa1a67c91dd7c61a94e80b719c26e48585874c1b
Fernando OR <fernando.or6@gmail.com>
2024-01-23 09:16:42 -0300

    feat: add interator to show cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MarvelApiService } from '../services/marvelApi.service';
+import { Character } from '../models/character';
+import { catchError } from 'rxjs';
+import { CardsComponent } from '../shared/cards/cards.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
-  ngOnInit(){
-    console.log('teste')
+export class HomeComponent implements OnInit {
+
+  public characters: Character[] = [];
+
+  constructor(private marvelApi: MarvelApiService) { }
+
+  ngOnInit(): void {
+    this.getCharacters();
   }
+
+  getCharacters() {
+
+    this.marvelApi.getAllCharacters()
+      .pipe(
+        catchError((err) => {
+          throw 'Erro ao consultar API: ' + err;
+        })
+      )
+      .subscribe((result) => {
+        console.log(result)
+        this.characters = result;
+      })
+
+  }
+
 }
