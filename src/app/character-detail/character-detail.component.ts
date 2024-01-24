25477109aea7c74e072faa8596c10b0bcd4caa5f
Fernando OR <fernando.or6@gmail.com>
2024-01-24 13:19:08 -0300

    feat: add confing observable and map result api

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable} from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+import { MarvelApiService } from '../services/marvelApi.service';
 import { SearchComponent } from '../shared/search/search.component';
+import { Character } from '../models/character';
 
 @Component({
   selector: 'app-character-detail',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [CommonModule, SearchComponent],
   templateUrl: './character-detail.component.html',
   styleUrl: './character-detail.component.scss'
 })
-export class CharacterDetailComponent {
+export class CharacterDetailComponent implements OnInit {
+
+  public character$!: Observable<Character>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private marvelApi: MarvelApiService
+  ) { }
+
+  ngOnInit(): void {
+    const characterId = this.route.snapshot.paramMap.get('id');
+    this.getCharacterById(characterId);
+  }
+
+  getCharacterById(characterId: unknown) {
+    this.character$ = this.marvelApi.getById(characterId)
+  }
 
 }
