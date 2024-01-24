7f8a45e2db55ed85ecbb6e431360a581cf392e88
Fernando OR <fernando.or6@gmail.com>
2024-01-24 11:19:37 -0300

    feat: add configuration routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { HomeComponent } from './home/home.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'character-detail/:id',
+    component: CharacterDetailComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,9 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    HeaderComponent,
-    HomeComponent
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
