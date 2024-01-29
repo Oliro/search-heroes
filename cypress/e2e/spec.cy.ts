4dc654eb6470a35f8769b53f6e1f7070f5ecc07f
Fernando OR <fernando.or6@gmail.com>
2024-01-29 18:13:28 -0300

    test: add teste for search character  and open details and home url

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,18 @@
 describe('My First Test', () => {
+
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('app is running!')
-  })
+    cy.contains('EXPLORE O UNIVERSO')
+  });
+
+  it('Search and open character details', () => {
+    cy.visit('/');
+    cy.contains('EXPLORE O UNIVERSO')
+    cy.get('#searchCharacter').type('spider-girl');
+    cy.wait(2500);
+    cy.get('#characterDetails').click();
+    cy.url().should('contain', '/character-detail/');
+  });
+
 })
+
